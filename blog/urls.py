f56646517217becca776b58d0f80e0c0ddff35db
Jon Winn <brainboy@webserv1.brainboy.mooo.com>
2015-11-28 08:57:43 -0700

    Jon - fixing urls files and expanding with patterns and autodiscover.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+"""
+ blog url definitions
+"""
+from django.conf.urls import patterns, include, url
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.post_list, name='post_list'),
+)
